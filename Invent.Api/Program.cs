abd0a8fa3c14629427858eb03eca66922f4826fa
Pablo Ribeiro <pabloribeiro.cont@gmail.com>
2025-06-07 20:56:12 -0300

    Adição do CORS

--- Invent.Api/Program.cs
+++ Invent.Api/Program.cs
@@ -2,21 +2,39 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Swagger
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()  // Permite requisições de qualquer origem
+               .AllowAnyMethod()  // Permite qualquer método HTTP (GET, POST, PUT, DELETE)
+               .AllowAnyHeader(); // Permite que a requisição tenha qualquer cabeçalho
+    });
+});
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Registro do repositório e controllers
+// Registro do repositório
 builder.Services.AddSingleton<IEquipamentoRepository, InMemoryEquipamentoRepository>();
-builder.Services.AddControllers();
+
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();

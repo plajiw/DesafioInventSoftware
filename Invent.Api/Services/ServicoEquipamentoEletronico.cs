e1e05b9b800e76089aeece3caa82d1d2afa772a4
Pablo Ribeiro <pabloribeiro.cont@gmail.com>
2025-06-24 15:11:45 -0300

    Update ServicoEquipamentoEletronico.cs

--- Invent.Api/Services/ServicoEquipamentoEletronico.cs
+++ Invent.Api/Services/ServicoEquipamentoEletronico.cs
@@ -1,63 +1,66 @@
 ﻿using FluentValidation;
 using Invent.Api.Data;
 using Invent.Api.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Invent.Api.Services
 {
-    // Implementamos a lógica e validação de negócio
-    public class ServicoEquipamentoEletronico
+    // Implementamos a lógica e validação de negócio
+    public class ServicoEquipamentoEletronico
     {
-        // Classe para o repositório
-        private readonly RavenDbEquipamentoRepositorio _repositorio;
+        // Dependência no repositório via interface
+        private readonly IEquipamentoRepositorio _repositorio;
 
-        // Classe para fazer as validações das regras de negócios sobre o EquipamentoEletronico 
-        private readonly IValidator<EquipamentoEletronico> _validador;
+        // Classe para fazer as validações das regras de negócios sobre o EquipamentoEletronico 
+        private readonly IValidator<EquipamentoEletronico> _validador;
 
-        // Construtor
-        public ServicoEquipamentoEletronico(RavenDbEquipamentoRepositorio repositorio, IValidator<EquipamentoEletronico> validador)
+        // Construtor
+        public ServicoEquipamentoEletronico(IEquipamentoRepositorio repositorio, IValidator<EquipamentoEletronico> validador)
         {
             _repositorio = repositorio;
             _validador = validador;
         }
 
-        // Criar Equipamento
-        public async Task<EquipamentoEletronico> Criar(EquipamentoEletronico equipamentoParaCriar)
+        // Criar Equipamento
+        public async Task<EquipamentoEletronico> Criar(EquipamentoEletronico equipamentoParaCriar)
         {
-            // Validação dos dados
-            _validador.ValidateAndThrow(equipamentoParaCriar);
+            // Validação dos dados
+            _validador.ValidateAndThrow(equipamentoParaCriar);
             return await _repositorio.CriarEquipamento(equipamentoParaCriar);
         }
 
-        // Atualizar por Id
-        public async Task<EquipamentoEletronico?> Atualizar(string id, EquipamentoEletronico dadosParaAtualizar)
+        // Atualizar por Id
+        public async Task<EquipamentoEletronico?> Atualizar(string id, EquipamentoEletronico dadosParaAtualizar)
         {
-            // Validar se o Id existe
-            if (id != dadosParaAtualizar.Id)
+            // Validar se o Id existe
+            if (id != dadosParaAtualizar.Id)
             {
+                // Colocar no Controller
                 throw new ValidationException("O Id da rota não corresponde ao ID no corpo da requisição.");
             }
 
             // Valida os dados do objeto
             _validador.ValidateAndThrow(dadosParaAtualizar);
 
-            // Registra no repositório
-            return await _repositorio.Atualizar(dadosParaAtualizar);
+            // Registra no repositório
+            return await _repositorio.Atualizar(dadosParaAtualizar);
         }
 
-        // Obter todos os equipamentos chamando o repositório
-        public async Task<IEnumerable<EquipamentoEletronico>> ObterTodos()
+        // Obter todos os equipamentos chamando o repositório
+        public async Task<IEnumerable<EquipamentoEletronico>> ObterTodos()
         {
             return await _repositorio.ObterTodos();
         }
 
-        // Obter equipamento por id
-        public async Task<EquipamentoEletronico?> ObterPorId(string idDoEquipamento)
+        // Obter equipamento por id
+        public async Task<EquipamentoEletronico?> ObterPorId(string idDoEquipamento)
         {
             return await _repositorio.ObterPorId(idDoEquipamento);
         }
 
-        // Remover
-        public async Task<bool> RemoverPorId(string id)
+        // Remover
+        public async Task<bool> RemoverPorId(string id)
         {
             return await _repositorio.RemoverPorId(id);
         }

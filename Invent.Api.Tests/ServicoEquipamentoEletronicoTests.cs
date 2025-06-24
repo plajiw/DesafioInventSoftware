dff3ca0e3090bab59b658bcc130477ff94b2ff57
plajiw <pabloribeiro.cont@gmail.com>
2025-06-24 15:21:56 -0300

    Criação ServicoEquipamentoEletronicoTests

--- Invent.Api.Tests/ServicoEquipamentoEletronicoTests.cs
+++ Invent.Api.Tests/ServicoEquipamentoEletronicoTests.cs
@@ -8,5 +8,38 @@ namespace Invent.Api.Tests
 {
     public class ServicoEquipamentoEletronicoTests
     {
+        // classe ServicoEquipamentoEletronico precisa de um repositório e validador para salvar os dados
+        private readonly Mock<IEquipamentoRepositorio> _mockRepositorio;
+        private readonly IValidator<EquipamentoEletronico> _validador;
+        private readonly ServicoEquipamentoEletronico _servico; // Objeto testado
+
+        // Construtor
+        public ServicoEquipamentoEletronicoTests()
+        {
+            _mockRepositorio = new Mock<IEquipamentoRepositorio>(); // Instancia do repositorio
+            _validador = new EquipamentoEletronicoValidador(); // Instancia do validador
+
+            _servico = new ServicoEquipamentoEletronico(_mockRepositorio.Object, _validador);
+        }
+
+        // Criação inválida, equipamento sem nome
+        [Fact]
+        public async Task Criar_Deve_Lancar_Excecao_Quando_Equipamento_Invalido()
+        {
+            // Arrange
+            var equipamento = new EquipamentoEletronico
+            {
+                Nome = "",
+                Tipo = "Teste",
+                QuantidadeEmEstoque = 1000
+            };
+
+            // Act
+            await Assert.ThrowsAsync<ValidationException>(() => _servico.Criar(equipamento));
+
+            // Assert
+            // Verificamos que o repositório nunca foi chamado, pois validação falhou
+            _mockRepositorio.Verify(repo => repo.CriarEquipamento(It.IsAny<EquipamentoEletronico>()), Times.Never);
+        }
     }
 }

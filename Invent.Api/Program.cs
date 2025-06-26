ec97ba83f7166a1f9648540b1b4808d364900ee4
plajiw <pabloribeiro.cont@gmail.com>
2025-06-26 16:08:07 -0300

    feat: adicionar listagem de equipamentos eletrônicos

--- Invent.Api/Program.cs
+++ Invent.Api/Program.cs
@@ -1,39 +1,60 @@
-﻿using FluentValidation;
-using FluentValidation.AspNetCore;
+﻿using FluentValidation.AspNetCore;
 using Invent.Api;
-using Invent.Api.Data;
-using Invent.Api.Models;
-using Invent.Api.Services;
+using Microsoft.AspNetCore.StaticFiles;
 using Raven.Client.Documents;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", builder =>
-    {
-        builder.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
-    });
-});
-
+// 1. Serviços de aplicação e validação
+ModuloInjecaoAplicacao.BindServices(builder.Services);
+builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddControllers();
+
+// 2. Swagger e CORS
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-ModuloInjecaoAplicacao.BindServices(builder.Services);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy => policy
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
 
 var app = builder.Build();
 
+// 3. Pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+// 4. Arquivos estáticos (UI5)
+app.UseStaticFiles(new StaticFileOptions
+{
+    ContentTypeProvider = new FileExtensionContentTypeProvider
+    {
+        Mappings = { [".properties"] = "application/x-msdownload" }
+    }
+});
+
+app.UseStaticFiles();    // Serve wwwroot/**
+
+// 5. Routing, CORS e Autorização
+app.UseRouting();
 app.UseCors("AllowAll");
 app.UseAuthorization();
-app.MapControllers();
+
+// 6. Endpoints
+app.MapControllers();               // API em /api/...
+app.MapFallbackToFile("index.html"); // Qualquer rota não mapeada retorna index
 
 app.Run();
---
+++ Invent.Api/wwwroot/controller/EquipamentoLista.controller.js
@@ -0,0 +1,18 @@
+﻿sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
+    "use strict";
+
+    return Controller.extend("ui5.walkthrough.controller.EquipamentoLista", {
+
+        onInit() {
+        
+            fetch("https://localhost:7178/api/EquipamentosControlador")
+                .then(r => r.json())
+                .then(data => this.getView().setModel(new JSONModel(data), "equipamentos"  ))
+                .then(() => this.getView().getModel("equipamentos").getData());
+
+        }
+    });
+});

d7c378dd7224f785f12dc6812a5c7068b72c010d
plajiw <pabloribeiro.cont@gmail.com>
2025-06-27 16:28:45 -0300

    Revert "listar equipamentos"
    This reverts commit 07b23d4db9716ff1c73a30ba9f007f6092b57d88.

--- Invent.Api/wwwroot/controller/EquipamentoLista.controller.js
+++ Invent.Api/wwwroot/controller/EquipamentoLista.controller.js
@@ -1,29 +1,29 @@
 ﻿sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/ui/model/json/JSONModel",
+  "sap/ui/core/format/DateFormat",
   "sap/ui/model/Filter",
   "sap/ui/model/FilterOperator",
-  "../model/formatter"
-], (Controller, JSONModel, Filter, FilterOperator, formatter) => {
+  "ui5/walkthrough/model/formatter"
+], (Controller, JSONModel, DateFormat, Filter, FilterOperator, formatter) => {
   "use strict";
 
-  return Controller.extend("ui5.gestaoequipamento.controller.EquipamentoLista", {
-    formatter: formatter,  // disponibiliza formater
+  return Controller.extend("ui5.walkthrough.controller.EquipamentoLista", {
+    formatter,  // disponibiliza no XML
 
-    onInit: function () {
+     onInit () {
       // cria o modelo vazio e já o associa à View
       var oModel = new JSONModel([]);
       this.getView().setModel(oModel, "equipamentos");
-
+      
       // carrega dados do serviço
       fetch("https://localhost:7178/api/EquipamentosControlador")
         .then(res => res.json())
         .then(data => this.getView().getModel("equipamentos").setData(data))
         .catch(err => console.error(err));
-
     },
 
-    onSearch: function (oEvent) {
+    onSearch(oEvent) {
       const sQuery = oEvent.getParameter("query");
       const oBinding = this.byId("tabelaEquipamentos").getBinding("items");
 
--- Invent.Api/wwwroot/model/formatter.js
+++ Invent.Api/wwwroot/model/formatter.js
@@ -1,12 +1,9 @@
-sap.ui.define([
-
-], function() {
+sap.ui.define([], function() {
   "use strict";
 
   return {
 
-    formatarDisponibilidade: function(bDisponibilidade) {
-      return bDisponibilidade ? "Sim" : "Não";
-    },
+    
+    
   };
 });

2b0574f76577e0d9e480792d85d18cd7ae8d32a4
plajiw <pabloribeiro.cont@gmail.com>
2025-06-27 15:39:59 -0300

    reset voltando codigo da listagem

---
+++ Invent.Api/wwwroot/model/formatter.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+
+], function() {
+  "use strict";
+
+  return {
+
+    formatarDisponibilidade: function(bDisponibilidade) {
+      return bDisponibilidade ? "Sim" : "Não";
+    },
+  };
+});

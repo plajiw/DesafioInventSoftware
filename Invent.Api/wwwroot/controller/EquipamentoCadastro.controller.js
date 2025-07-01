91fb342dd1893be1302ca511989b453d6f483099
plajiw <pabloribeiro.cont@gmail.com>
2025-07-01 13:55:02 -0300

    funcionalidade de cadastro

--- Invent.Api/wwwroot/controller/EquipamentoCadastro.controller.js
+++ Invent.Api/wwwroot/controller/EquipamentoCadastro.controller.js
@@ -1,26 +1,80 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], (Controller, JSONModel, MessageToast) => {
     "use strict";
 
-    const ROTA_PARA_LISTA = "listaEquipamento";
+    // Constantes
+    const NOME_MODELO_FORMULARIO = "equipamento";
+    const ROTA_LISTA = "listaEquipamento";
+    const ROTA_CADASTRO = "cadastroEquipamento";
+    const ROTA_DETALHE = "detalheEquipamento";
+    const NOME_CONTROLADOR_EQUIPAMENTOS = "Equipamentos";
+    const URL_BASE_API = "https://localhost:7178/api";
+    const ENDPOINT_EQUIPAMENTOS = `${URL_BASE_API}/${NOME_CONTROLADOR_EQUIPAMENTOS}`;
 
     return Controller.extend("ui5.gestaoequipamento.controller.EquipamentoCadastro", {
-
-        // Função de inicialização
         onInit: function () {
-            
+            // Cria um JSONModel vazio {} e associa à view como “equipamento”
+            this.getView().setModel(new JSONModel({}), NOME_MODELO_FORMULARIO);
+
+            // Obtem o roteador para poder reagir à rota de cadastro (Component.js -> manifest.json)
+            const oRoteador = this.getOwnerComponent().getRouter();
+            oRoteador.getRoute(ROTA_CADASTRO).attachPatternMatched(this._aoCoincidirRotaCadastro, this);
         },
 
-        // Retornar página
-        aoClicarEmVoltar: function () {
-            this.getOwnerComponent().getRouter().navTo(ROTA_PARA_LISTA);
-        },
-
-        // Salvar
         aoClicarEmSalvar: function () {
+            const oDadosDoFormulario = this.getView().getModel(NOME_MODELO_FORMULARIO).getData();
 
+            // Lógica de Cadastro (POST)
+            this._criarEquipamentoNaAPI(oDadosDoFormulario);
+        },
+
+        // Retornar para a página de lista
+        aoClicarEmVoltar: function () {
+            this.getOwnerComponent().getRouter().navTo(ROTA_LISTA);
+        },
+
+        // Função para reinicializar o estado do formulário
+        // Assim que acessar "cadastroEquipamento" o método é disparado
+        // Refinimos o modelo "equipamento" para um objeto vazio 
+        _aoCoincidirRotaCadastro: function () {
+            this.getView().getModel(NOME_MODELO_FORMULARIO).setData({});
+        },
+
+        // Função privada para encapsular a lógica de chamada da API para criar
+        _criarEquipamentoNaAPI: function (oDadosDoEquipamento) {
+            fetch(ENDPOINT_EQUIPAMENTOS, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(oDadosDoEquipamento)
+            })
+                .then(resposta => {
+                    if (!resposta.ok) {
+                        return resposta.json().then(erro => { throw new Error(erro.message || "Falha ao criar equipamento") });
+                    }
+                    return resposta.json();
+                })
+                .then(dadosCriados => {
+
+                    // 1. Navegamos para a tela de detalhes
+                    this._navegarParaDetalhes(dadosCriados.id);
+
+                    // Exibição do MessageToast
+                    setTimeout(() => {
+                        MessageToast.show("Equipamento cadastrado com sucesso!");
+                    }, 300);
+                })
+                .catch(erro => {
+                    MessageBox.error(`Ocorreu um erro ao tentar cadastrar: ${erro.message}`);
+                });
+        },
+
+        // Função privada para encapsular a lógica de navegação
+        _navegarParaDetalhes: function (sId) {
+            const oRoteador = this.getOwnerComponent().getRouter();
+            oRoteador.navTo(ROTA_DETALHE, { id: sId }, true); // O 'true' limpa o histórico
         }
-
     });
 });
--- Invent.Api/wwwroot/controller/EquipamentoDetalhe.controller.js
+++ Invent.Api/wwwroot/controller/EquipamentoDetalhe.controller.js
@@ -1,53 +1,71 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-      "../model/formatter"
-], (Controller, JSONModel, formatter) => {
+     "../model/formatter"
+], (Controller, JSONModel) => {
     "use strict";
 
-    const MODELO_DE_DETALHES_EQUIPAMENTOS = "detalhes";
-    const ROTA_PARA_LISTA = "listaEquipamento";
-    const ROTA_PARA_DETALHES = "detalheEquipamento";
-    const URL_BASE_API = "https://localhost:7178/api";
-    const NOME_CONTROLADOR_EQUIPAMENTOS = "Equipamentos";
-    const ENDPOINT_EQUIPAMENTOS = `${URL_BASE_API}/${NOME_CONTROLADOR_EQUIPAMENTOS}`;
+    // --- Constantes do Controller ---
+    const NOME_MODELO_DETALHES = "detalhes";
+    const NOME_ROTA_LISTA = "listaEquipamento";
+    const NOME_ROTA_DETALHES = "detalheEquipamento";
+    const NOME_ROTA_EDITAR = "editarEquipamento";
+    const ENDPOINT_EQUIPAMENTOS = "api/Equipamentos";
 
     return Controller.extend("ui5.gestaoequipamento.controller.EquipamentoDetalhe", {
-    formatter: formatter,
-
+        formatter: formatter,
         onInit: function () {
             // Cria um modelo JSON local para armazenar os dados do equipamento detalhado
             const oModelo = new JSONModel();
-            this.getView().setModel(oModelo, MODELO_DE_DETALHES_EQUIPAMENTOS);
+            this.getView().setModel(oModelo, NOME_MODELO_DETALHES);
 
-            // Obtem a rota e verifica os ID
+            // Obtem a rota e anexa o listener para quando ela for acessada
             const oRoteador = this.getOwnerComponent().getRouter();
-            oRoteador.getRoute(ROTA_PARA_DETALHES).attachPatternMatched(this._aoCoincidirRota, this);
+            oRoteador.getRoute(NOME_ROTA_DETALHES).attachPatternMatched(this._aoCoincidirRota, this);
         },
 
         // Função para voltar para a lista de equipamentos
         aoClicarEmVoltar: function () {
-            this.getOwnerComponent().getRouter().navTo(ROTA_PARA_LISTA);
+            this.getOwnerComponent().getRouter().navTo(NOME_ROTA_LISTA, {}, true);
         },
 
-        // Função para obter o ID
+        // Função para navegar para a tela de edição
+        aoClicarEmEditar: function () {
+            // Obtemos o modelo com os dados atuais do equipamento
+            const oModelo = this.getView().getModel(NOME_MODELO_DETALHES);
+
+            // Obtermos o ID do equipamento a partir do modelo
+            const idDoEquipamento = oModelo.getProperty("/id");
+
+            // Obtemos a instância do roteador
+            const oRoteador = this.getOwnerComponent().getRouter();
+
+            // Navegamos para a rota de edição (cadastro), passando o ID como parâmetro
+            oRoteador.navTo(NOME_ROTA_EDITAR, {
+                id: idDoEquipamento
+            });
+        },
+
+        // Função para remover
+        aoClicarEmRemover: function () {
+        },
+
+        // Função executada quando a rota de detalhes é acessada
         _aoCoincidirRota: function (oEvento) {
-            // Pega o ID dos argumentos do evento
+            // Pega o ID dos argumentos do evento da URL
             const id = oEvento.getParameter("arguments").id;
-            console.log("ID do Equipamento a ser buscado:", id);
 
             // Obtemos o modelo "detalhes" da view
-            const oModeloDetalhes = this.getView().getModel(MODELO_DE_DETALHES_EQUIPAMENTOS);
+            const oModeloDetalhes = this.getView().getModel(NOME_MODELO_DETALHES);
 
-            // Construção da URL
+            // Construção da URL da API
             const urlApi = `${ENDPOINT_EQUIPAMENTOS}/${id}`;
-            console.log("Endpoint acessado:", urlApi);
 
-            // Chamada fetch para API
+            // Chamada fetch para a API para buscar os dados
             fetch(urlApi)
                 .then(res => res.json())
-                .then(dados => { oModeloDetalhes.setData(dados)})
-                .catch(err => console.error(err));
+                .then(dadosRecebidos => { oModeloDetalhes.setData(dadosRecebidos) })
+                .catch(err => console.error("Erro ao buscar detalhes do equipamento:", err));
         }
     });
 });

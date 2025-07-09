e18465df407dedd53b59fac881e39191dd8a18b3
Pablo Ribeiro <pabloribeiro.cont@gmail.com>
2025-07-09 00:27:50 -0300

    Ajustar teste de euNaoConfirmoRemocao

--- Invent.Api/wwwroot/test/integration/pages/equipamentos/Detalhe.js
+++ Invent.Api/wwwroot/test/integration/pages/equipamentos/Detalhe.js
@@ -1,121 +1,84 @@
 // webapp/test/integration/pages/Detalhe.js
 sap.ui.define([
-  "sap/ui/test/Opa5",
-  "sap/ui/test/matchers/I18NText",
-  "sap/ui/test/matchers/PropertyStrictEquals",
-  "sap/ui/test/actions/Press"
+    "sap/ui/test/Opa5",
+    "sap/ui/test/matchers/I18NText",
+    "sap/ui/test/matchers/PropertyStrictEquals",
+    "sap/ui/test/actions/Press"
 ], function (Opa5, I18NText, PropertyStrictEquals, Press) {
-  "use strict";
+    "use strict";
 
-  const VIEW_NAME = "EquipamentoDetalhe";
+    const VIEW_NAME = "EquipamentoDetalhe";
 
-  Opa5.createPageObjects({
-    naPaginaDeDetalheDeEquipamento: {
-
-      actions: {
-        euClicoEmVoltar: function () {
-          return this.waitFor({
-            controlType: "sap.m.Button",
-            viewName: VIEW_NAME,
-            matchers: new PropertyStrictEquals({
-              name: "type",
-              value: "Back"
-            }),
-            actions: new Press(),
-            success: function () {
-              Opa5.assert.ok(true, "Cliquei em Voltar na tela de Detalhe");
+    Opa5.createPageObjects({
+        naPaginaDeDetalheDeEquipamento: {
+            actions: {
+                euClicoEmVoltar: function () {
+                    return this.waitFor({
+                        controlType: "sap.m.Button",
+                        viewName: VIEW_NAME,
+                        matchers: new PropertyStrictEquals({
+                            name: "type",
+                            value: "Back"
+                        }),
+                        actions: new Press()
+                    });
+                },
+                euClicoEmRemover: function () {
+                    return this.waitFor({
+                        controlType: "sap.m.Button",
+                        viewName: VIEW_NAME,
+                        matchers: new I18NText({
+                            propertyName: "text",
+                            key: "botaoRemover"
+                        }),
+                        actions: new Press()
+                    });
+                },
+                euClicoEmEditar: function () {
+                    return this.waitFor({
+                        controlType: "sap.m.Button",
+                        viewName: VIEW_NAME,
+                        matchers: new I18NText({
+                            propertyName: "text",
+                            key: "botaoEditar"
+                        }),
+                        actions: new Press()
+                    });
+                },
+                euConfirmoRemocao: function () {
+                    return this.waitFor({
+                        controlType: "sap.m.Button",
+                        matchers: new PropertyStrictEquals({
+                            name: "text",
+                            value: "Sim"
+                        }),
+                        actions: new Press()
+                    });
+                },
+                euNaoConfirmoRemocao: function () {
+                    const INDICE_BOTAO_NAO = 1;
+                    return this.waitFor({
+                        controlType: "sap.m.Button",
+                        searchOpenDialogs: true, // Limita a busca aos controles dentro do diálogo aberto
+                        success: function (aButtons) { // [Botão 0: ID=__mbox-btn-0, Texto=Sim, Botão 1: ID=__mbox-btn-1, Texto=Não"]
+                            aButtons[INDICE_BOTAO_NAO].firePress();
+                            Opa5.assert.ok(true, "Botão 'Não' clicado no diálogo.");
+                        },
+                    });
+                }
             },
-            errorMessage: "Não encontrei o botão Voltar na tela de Detalhe."
-          });
-        },
-        euClicoEmRemover: function()
-        {
-            return this.waitFor({
-                controlType: "sap.m.Button",
-                viewName: VIEW_NAME,
-                matchers: new I18NText({
-                    propertyName: "text",
-                    key: "botaoRemover"
-                }),
-                actions: new Press(),
-                success: function()
-                {
-                    Opa5.assert.ok(true, "Cliquei em Remover na tela de Detalhe");
-                },
-                errorMessage: "Não encontrei o botão Remover na tela de Detalhe."
-            });
-        },
-        euClicoEmEditar: function()
-        {
-            return this.waitFor({
-                controlType: "sap.m.Button",
-                viewName: VIEW_NAME,
-                matchers: new I18NText({
-                    propertyName: "text",
-                    key: "botaoEditar"
-                }),
-                actions: new Press(),
-                success: function()
-                {
-                    Opa5.assert.ok(true, "Cliquei em Editar na tela de Detalhe");
-                },
-                errorMessage: "Não encontrei o botão Editar na tela de Detalhe."
-            });
-        },
-
-        euConfirmoRemocao: function()
-        {
-            return this.waitFor({
-                controlType: "sap.m.Button",
-                // Ancestor
-                matchers: new PropertyStrictEquals({
-                    name: "text",
-                    value: "Sim"
-                }),
-                actions: new Press(),
-                success: function()
-                {
-                    Opa5.assert.ok(true, "Confirmou remoção do equipamento.");
-                },
-                errorMessage: "Não foi possível localizar a opção para confirmar a remoção."
-            });
-        },
-
-        euNaoConfirmoRemocao: function()
-        {
-            return this.waitFor({
-                controlType: "sap.m.Button",
-                matchers: new PropertyStrictEquals({
-                    name: "text",
-                    value: "Não"
-                }),
-                actions: new Press(),
-                success: function()
-                {
-                    Opa5.assert.ok(true, "Confirmou a não remoção do equipamento.");
-                },
-                errorMessage: "Não foi possível localizar a opção para negar a remoção."
-            });
+            assertions: {
+                paginaDeDetalheAberta: function () {
+                    return this.waitFor({
+                        controlType: "sap.m.Page",
+                        viewName: VIEW_NAME,
+                        matchers: new I18NText({
+                            propertyName: "title",
+                            key: "tituloPaginaDetalhes"
+                        })
+                    });
+                }
+            }
         }
-      },
-
-      assertions: {
-        paginaDeDetalheAberta: function () {
-          return this.waitFor({
-            controlType: "sap.m.Page",
-            viewName: VIEW_NAME,
-            matchers: new I18NText({
-              propertyName: "title",
-              key: "tituloPaginaDetalhes"
-            }),
-            success: function () {
-              Opa5.assert.ok(true, "Tela de Detalhe acessada corretamente.");
-            },
-            errorMessage: "Tela de Detalhe não abriu como esperado."
-          });
-        }
-      }
-
-    }
-  });
+    });
 });

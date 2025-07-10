1cce63561718a8e174dccef248441f2b65f2d840
plajiw <pabloribeiro.cont@gmail.com>
2025-07-10 13:55:23 -0300

    Corrigir funcionalidades de validação, cadastro e edição

--- Invent.Api/Enums/TipoEquipamentoEletronico.cs
+++ Invent.Api/Enums/TipoEquipamentoEletronico.cs
@@ -2,17 +2,17 @@
 {
     public enum TipoEquipamentoEletronico
     {
-        Desktop = 0,
-        Notebook = 1,
-        Smartphone = 2,
-        Tablet = 3,
-        TV = 4,
-        Monitor = 5,
-        Teclado = 6,
-        Mouse = 7,
-        Impressora = 8,
-        Webcam = 9,
-        Headset = 10,
-        Camera = 11,
+        Desktop,
+        Notebook,
+        Smartphone,
+        Tablet,
+        TV,
+        Monitor,
+        Teclado,
+        Mouse,
+        Impressora,
+        Webcam,
+        Headset,
+        Camera,
     }
 }

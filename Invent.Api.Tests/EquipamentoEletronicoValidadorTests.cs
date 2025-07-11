c861e67b13cab6f8328d6dfac73ae1458a60d6d9
plajiw <pabloribeiro.cont@gmail.com>
2025-07-11 11:16:27 -0300

    Remover comentários

--- Invent.Api.Tests/EquipamentoEletronicoValidadorTests.cs
+++ Invent.Api.Tests/EquipamentoEletronicoValidadorTests.cs
@@ -1,114 +1,114 @@
-﻿//using FluentValidation;
-//using Invent.Api.Models;
+﻿using FluentValidation;
+using Invent.Api.Models;
 
-//namespace Invent.Api.Tests
-//{
-//    public class EquipamentoEletronicoValidadorTests
-//    {
-//        private readonly EquipamentoEletronicoValidador _validador = new EquipamentoEletronicoValidador();
+namespace Invent.Api.Tests
+{
+    public class EquipamentoEletronicoValidadorTests
+    {
+        private readonly EquipamentoEletronicoValidador _validador = new EquipamentoEletronicoValidador();
 
-//        [Fact]
-//        public void Nao_Deve_Lancar_Excecao_Null_Quando_Incluir_Equipamento_Valido()
-//        {
-//            var equipamento = new EquipamentoEletronico
-//            {
-//                Nome = "monitor Samsung T350",
-//                Tipo = "Monitor",
-//                QuantidadeEmEstoque = 50
-//            };
+        [Fact]
+        public void Nao_Deve_Lancar_Excecao_Null_Quando_Incluir_Equipamento_Valido()
+        {
+            var equipamento = new EquipamentoEletronico
+            {
+                Nome = "monitor Samsung T350",
+                Tipo = "Monitor",
+                QuantidadeEmEstoque = 50
+            };
 
-//            var excecao = Record.Exception(() => _validador.ValidateAndThrow(equipamento));
+            var excecao = Record.Exception(() => _validador.ValidateAndThrow(equipamento));
 
-//            Assert.Null(excecao);
-//        }
+            Assert.Null(excecao);
+        }
 
-//        [Fact]
-//        public void Deve_Falhar_Quando_Nome_Esta_Vazio()
-//        {
-//            var equipamento = new EquipamentoEletronico
-//            {
-//                Nome = "",
-//                Tipo = "Teclado",
-//                QuantidadeEmEstoque = 7
-//            };
+        [Fact]
+        public void Deve_Falhar_Quando_Nome_Esta_Vazio()
+        {
+            var equipamento = new EquipamentoEletronico
+            {
+                Nome = "",
+                Tipo = "Teclado",
+                QuantidadeEmEstoque = 7
+            };
 
-//            string erroObrigatorio = "O nome do equipamento é obrigatório.";
-//            string erroTamanho = "O nome deve ter entre 3 e 100 caracteres.";
+            string erroObrigatorio = "O nome do equipamento é obrigatório.";
+            string erroTamanho = "O nome deve ter entre 3 e 100 caracteres.";
 
-//            var excecao = Assert.Throws<ValidationException>(() => _validador.ValidateAndThrow(equipamento));
+            var excecao = Assert.Throws<ValidationException>(() => _validador.ValidateAndThrow(equipamento));
 
-//            Assert.Contains(erroObrigatorio, excecao.Message);
-//            Assert.Contains(erroTamanho, excecao.Message);
-//        }
+            Assert.Contains(erroObrigatorio, excecao.Message);
+            Assert.Contains(erroTamanho, excecao.Message);
+        }
 
-//        [Fact]
-//        public void Deve_Falhar_Quando_Nome_Conter_Menos_Que_Tres_Caracteres()
-//        {
-//            var equipamento = new EquipamentoEletronico
-//            {
-//                Nome = "Ab",
-//                Tipo = "Teclado",
-//                QuantidadeEmEstoque = 7
-//            };
+        [Fact]
+        public void Deve_Falhar_Quando_Nome_Conter_Menos_Que_Tres_Caracteres()
+        {
+            var equipamento = new EquipamentoEletronico
+            {
+                Nome = "Ab",
+                Tipo = "Teclado",
+                QuantidadeEmEstoque = 7
+            };
 
-//            string erroTamanho = "O nome deve ter entre 3 e 100 caracteres.";
+            string erroTamanho = "O nome deve ter entre 3 e 100 caracteres.";
 
-//            var excecao = Assert.Throws<ValidationException>(() => _validador.ValidateAndThrow(equipamento));
+            var excecao = Assert.Throws<ValidationException>(() => _validador.ValidateAndThrow(equipamento));
 
-//            Assert.Contains(erroTamanho, excecao.Message);
-//        }
+            Assert.Contains(erroTamanho, excecao.Message);
+        }
 
-//        [Fact]
-//        public void Deve_Falhar_Quando_Nome_Conter_Mais_Que_Cem_Caracteres()
-//        {
-//            var equipamento = new EquipamentoEletronico
-//            {
-//                Nome = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-//                Tipo = "Teclado",
-//                QuantidadeEmEstoque = 7
-//            };
+        [Fact]
+        public void Deve_Falhar_Quando_Nome_Conter_Mais_Que_Cem_Caracteres()
+        {
+            var equipamento = new EquipamentoEletronico
+            {
+                Nome = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                Tipo = "Teclado",
+                QuantidadeEmEstoque = 7
+            };
 
-//            string erroTamanho = "O nome deve ter entre 3 e 100 caracteres.";
+            string erroTamanho = "O nome deve ter entre 3 e 100 caracteres.";
 
-//            var excecao = Assert.Throws<ValidationException>(() => _validador.ValidateAndThrow(equipamento));
+            var excecao = Assert.Throws<ValidationException>(() => _validador.ValidateAndThrow(equipamento));
 
-//            Assert.Contains(erroTamanho, excecao.Message);
-//        }
+            Assert.Contains(erroTamanho, excecao.Message);
+        }
 
-//        [Fact]
-//        public void Deve_Falhar_Quando_Tipo_Esta_Vazio()
-//        {
-//            var equipamento = new EquipamentoEletronico
-//            {
-//                Nome = "Teclado Mecânico",
-//                Tipo = "",
-//                QuantidadeEmEstoque = 13
-//            };
+        [Fact]
+        public void Deve_Falhar_Quando_Tipo_Esta_Vazio()
+        {
+            var equipamento = new EquipamentoEletronico
+            {
+                Nome = "Teclado Mecânico",
+                Tipo = "",
+                QuantidadeEmEstoque = 13
+            };
 
-//            string mensagemDeErro = "O tipo do equipamento é obrigatório.";
+            string mensagemDeErro = "O tipo do equipamento é obrigatório.";
 
-//            var excecao = Assert.Throws<ValidationException>(() => _validador.ValidateAndThrow(equipamento));
+            var excecao = Assert.Throws<ValidationException>(() => _validador.ValidateAndThrow(equipamento));
 
-//            Assert.Contains(mensagemDeErro, excecao.Message);
-//        }
+            Assert.Contains(mensagemDeErro, excecao.Message);
+        }
 
-//        [Theory]
-//        [InlineData(-10001, "A quantidade precisa ser maior ou igual a zero.")]
-//        [InlineData(-1, "A quantidade precisa ser maior ou igual a zero.")]
-//        [InlineData(10001, "A quantidade em estoque não pode exceder 10.000 unidades.")]
-//        [InlineData(100000, "A quantidade em estoque não pode exceder 10.000 unidades.")]
-//        public void Deve_Falhar_Quantidade_Em_Estoque_Invalida(int quantidadeInvalida, string mensagemDeErro)
-//        {
-//            var equipamento = new EquipamentoEletronico
-//            {
-//                Nome = "Webcam Logitech",
-//                Tipo = "Webcam",
-//                QuantidadeEmEstoque = quantidadeInvalida
-//            };
+        [Theory]
+        [InlineData(-10001, "A quantidade precisa ser maior ou igual a zero.")]
+        [InlineData(-1, "A quantidade precisa ser maior ou igual a zero.")]
+        [InlineData(10001, "A quantidade em estoque não pode exceder 10.000 unidades.")]
+        [InlineData(100000, "A quantidade em estoque não pode exceder 10.000 unidades.")]
+        public void Deve_Falhar_Quantidade_Em_Estoque_Invalida(int quantidadeInvalida, string mensagemDeErro)
+        {
+            var equipamento = new EquipamentoEletronico
+            {
+                Nome = "Webcam Logitech",
+                Tipo = "Webcam",
+                QuantidadeEmEstoque = quantidadeInvalida
+            };
 
-//            var excecao = Assert.Throws<ValidationException>(() => _validador.ValidateAndThrow(equipamento));
+            var excecao = Assert.Throws<ValidationException>(() => _validador.ValidateAndThrow(equipamento));
 
-//            Assert.Contains(mensagemDeErro, excecao.Message);
-//        }
-//    }
-//}
+            Assert.Contains(mensagemDeErro, excecao.Message);
+        }
+    }
+}
--- Invent.Api.Tests/ServicoEquipamentoEletronicoTests.cs
+++ Invent.Api.Tests/ServicoEquipamentoEletronicoTests.cs
@@ -1,82 +1,82 @@
-﻿//using FluentValidation;
-//using Invent.Api.Data;
-//using Invent.Api.Models;
-//using Invent.Api.Services;
-//using Moq;
+﻿using FluentValidation;
+using Invent.Api.Data;
+using Invent.Api.Models;
+using Invent.Api.Services;
+using Moq;
 
-//namespace Invent.Api.Tests
-//{
-//    public class ServicoEquipamentoEletronicoTests
-//    {
-//        private readonly Mock<IEquipamentoRepositorio> _mockRepositorio;
-//        private readonly IValidator<EquipamentoEletronico> _validador;
-//        private readonly ServicoEquipamentoEletronico _servico;
+namespace Invent.Api.Tests
+{
+    public class ServicoEquipamentoEletronicoTests
+    {
+        private readonly Mock<IEquipamentoRepositorio> _mockRepositorio;
+        private readonly IValidator<EquipamentoEletronico> _validador;
+        private readonly ServicoEquipamentoEletronico _servico;
 
-//        // Construtor
-//        public ServicoEquipamentoEletronicoTests()
-//        {
-//            _mockRepositorio = new Mock<IEquipamentoRepositorio>();
-//            _validador = new EquipamentoEletronicoValidador();
+        // Construtor
+        public ServicoEquipamentoEletronicoTests()
+        {
+            _mockRepositorio = new Mock<IEquipamentoRepositorio>();
+            _validador = new EquipamentoEletronicoValidador();
 
-//            _servico = new ServicoEquipamentoEletronico(_mockRepositorio.Object, _validador);
-//        }
+            _servico = new ServicoEquipamentoEletronico(_mockRepositorio.Object, _validador);
+        }
 
-//        [Fact]
-//        public async Task Criar_Deve_Chamar_Repositorio_Quando_Equipamento_Valido()
-//        {
-//            var equipamento = new EquipamentoEletronico
-//            {
-//                Nome = "Nome Teste",
-//                Tipo = "Tipo Teste",
-//                QuantidadeEmEstoque = 1001
-//            };
+        [Fact]
+        public async Task Criar_Deve_Chamar_Repositorio_Quando_Equipamento_Valido()
+        {
+            var equipamento = new EquipamentoEletronico
+            {
+                Nome = "Nome Teste",
+                Tipo = "Tipo Teste",
+                QuantidadeEmEstoque = 1001
+            };
 
-//            _mockRepositorio.Setup(repo => repo.CriarEquipamento(equipamento)).ReturnsAsync(equipamento);
+            _mockRepositorio.Setup(repo => repo.CriarEquipamento(equipamento)).ReturnsAsync(equipamento);
 
-//            var excecao = await _servico.Criar(equipamento);
+            var excecao = await _servico.Criar(equipamento);
 
-//            Assert.NotNull(excecao);
-//            _mockRepositorio.Verify(repo => repo.CriarEquipamento(equipamento), Times.Once());
-//        }
+            Assert.NotNull(excecao);
+            _mockRepositorio.Verify(repo => repo.CriarEquipamento(equipamento), Times.Once());
+        }
 
-//        [Fact]
-//        public async Task Criar_Deve_Lancar_Excecao_Quando_Equipamento_Invalido()
-//        {
-//            var equipamento = new EquipamentoEletronico
-//            {
-//                Nome = "",
-//                Tipo = "Teste",
-//                QuantidadeEmEstoque = 1000
-//            };
+        [Fact]
+        public async Task Criar_Deve_Lancar_Excecao_Quando_Equipamento_Invalido()
+        {
+            var equipamento = new EquipamentoEletronico
+            {
+                Nome = "",
+                Tipo = "Teste",
+                QuantidadeEmEstoque = 1000
+            };
 
-//            string erroObrigatorio = "O nome do equipamento é obrigatório.";
-//            string erroTamanho = "O nome deve ter entre 3 e 100 caracteres.";
+            string erroObrigatorio = "O nome do equipamento é obrigatório.";
+            string erroTamanho = "O nome deve ter entre 3 e 100 caracteres.";
 
-//            var excecao = await Assert.ThrowsAsync<ValidationException>(() => _servico.Criar(equipamento));
+            var excecao = await Assert.ThrowsAsync<ValidationException>(() => _servico.Criar(equipamento));
 
-//            Assert.Contains(erroObrigatorio, excecao.Message);
-//            Assert.Contains(erroTamanho, excecao.Message);
+            Assert.Contains(erroObrigatorio, excecao.Message);
+            Assert.Contains(erroTamanho, excecao.Message);
 
-//            _mockRepositorio.Verify(repo => repo.CriarEquipamento(It.IsAny<EquipamentoEletronico>()), Times.Never);
-//        }
+            _mockRepositorio.Verify(repo => repo.CriarEquipamento(It.IsAny<EquipamentoEletronico>()), Times.Never);
+        }
 
-//        [Theory]
-//        [InlineData(-1, "A quantidade precisa ser maior ou igual a zero.")]
-//        [InlineData(10001, "A quantidade em estoque não pode exceder 10.000 unidades.")]
-//        public async Task Criar_Deve_Lancar_Excecao_Quando_QuantidadeEmEstoque_Eh_Invalida(int quantidadeEstoque, string mensagemDeErro)
-//        {
-//            var equipamento = new EquipamentoEletronico
-//            {
-//                Nome = "Equipamento com Estoque Inválido",
-//                Tipo = "Teste de Teoria",
-//                QuantidadeEmEstoque = quantidadeEstoque
-//            };
+        [Theory]
+        [InlineData(-1, "A quantidade precisa ser maior ou igual a zero.")]
+        [InlineData(10001, "A quantidade em estoque não pode exceder 10.000 unidades.")]
+        public async Task Criar_Deve_Lancar_Excecao_Quando_QuantidadeEmEstoque_Eh_Invalida(int quantidadeEstoque, string mensagemDeErro)
+        {
+            var equipamento = new EquipamentoEletronico
+            {
+                Nome = "Equipamento com Estoque Inválido",
+                Tipo = "Teste de Teoria",
+                QuantidadeEmEstoque = quantidadeEstoque
+            };
 
-//            var excecao = await Assert.ThrowsAsync<ValidationException>(() => _servico.Criar(equipamento));
+            var excecao = await Assert.ThrowsAsync<ValidationException>(() => _servico.Criar(equipamento));
 
-//            Assert.Contains(mensagemDeErro, excecao.Message);
-//            _mockRepositorio.Verify(repo => repo.CriarEquipamento(It.IsAny<EquipamentoEletronico>()), Times.Never);
-//        }
+            Assert.Contains(mensagemDeErro, excecao.Message);
+            _mockRepositorio.Verify(repo => repo.CriarEquipamento(It.IsAny<EquipamentoEletronico>()), Times.Never);
+        }
 
-//    }
-//}
+    }
+}
